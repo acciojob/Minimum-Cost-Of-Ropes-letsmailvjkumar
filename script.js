742dc721b1634d11231d3b7e70bdc40f887f01be
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-20 10:25:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function calculateMinCost() {
-  //your code here
+document.getElementById("ropes-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const input = document.getElementById("ropes-input").value;
+    const ropes = input.split(",").map(Number);
+    const minimumCost = connectRopes(ropes);
+
+    // Display the result in the "result" div
+    document.getElementById("result").innerHTML = "Minimum cost: " + minimumCost;
+	function connectRopes(arr) {
   let totalCost = 0;
 
   // Sort the array in ascending order
@@ -9,20 +16,16 @@ function calculateMinCost() {
     // Extract the two smallest elements from the sorted array
     const min1 = arr.shift();
     const min2 = arr.shift();
-
     // Calculate the sum of the two smallest elements
     const sum = min1 + min2;
-
     // Add the sum to the total cost
     totalCost += sum;
-
     // Insert the sum back into the sorted array
     arr.push(sum);
-
     // Re-sort the array (optional, if you want to maintain the sorted order)
     arr.sort((a, b) => a - b);
   }
+  return totalCost;
+}
 
-  return totalCost; 
-  
-}  
+});
