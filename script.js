7a61bd58a7afa8752aae94ae0651107ee301e996
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-20 10:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function calculateMinCost() {
   //your code here
-  
-  
+  let totalCost = 0;
+
+  // Sort the array in ascending order
+  arr.sort((a, b) => a - b);
+
+  while (arr.length > 1) {
+    // Extract the two smallest elements from the sorted array
+    const min1 = arr.shift();
+    const min2 = arr.shift();
+
+    // Calculate the sum of the two smallest elements
+    const sum = min1 + min2;
+
+    // Add the sum to the total cost
+    totalCost += sum;
+
+    // Insert the sum back into the sorted array
+    arr.push(sum);
+
+    // Re-sort the array (optional, if you want to maintain the sorted order)
+    arr.sort((a, b) => a - b);
+  }
+
+  return totalCost; 
   
 }  
